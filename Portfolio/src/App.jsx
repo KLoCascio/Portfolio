f74c344841ed018815fb6723b3afe4bb0e92fb13
Kyndal LoCascio <KyndalLoCascio@gmail.com>
2024-01-16 20:57:40 -0500

    Portfolio 1.1

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -8,9 +8,7 @@ import Home from './components/Home'
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element ={<Home />} />
-      </Routes>
+      <Home />
     </>
   )
 }
